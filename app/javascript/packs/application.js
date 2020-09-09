9c1d469555958da074a40a998a38affec422fd03
harishmallow <harish@mallow-tech.com>
2020-09-10 03:31:25 +0530

    - Commented out select2 initialization

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,6 @@ import $ from 'jquery'
 import 'select2'
 import 'select2/dist/css/select2.css'
 
-$(document).on('turbolinks:load', function() {
-    $('.select2-multiple').select2();
-});
+// $(document).on('turbolinks:load', function() {
+//     $('.select2-multiple').select2();
+// });
